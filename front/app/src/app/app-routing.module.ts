7c9efbb083df1453c50aedaf000279cbcd4eecdd
T-H9703EnAc <t.h.1997.m.engineer.ac@gmail.com>
2022-09-11 17:06:55 +0900

    検索画面作成

--- front/app/src/app/app-routing.module.ts
+++ front/app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { Page1Component } from './pages/page1/page1.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'page1', component: Page1Component},
 ];
 
 @NgModule({
